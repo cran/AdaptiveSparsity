38e0afd32200cda8aaeba1cb6b6e2f2ff371fa2f
Kristen Zygmunt <krismz@sci.utah.edu>
2013-09-22 00:00:00 +0000

    version 1.3

--- R/aslm.R
+++ R/aslm.R
@@ -22,7 +22,7 @@ aslm.default <- function(x, y, ...)
     est$call <- match.call()
     # TODO how to handle formula and data etc. to support summary.aslm?
     est$data <- c(x, y)
-
+    est$contrasts <- attr(x, "contrasts")
     class(est) <- c("aslm","lm")
     est
   }
@@ -31,9 +31,40 @@ getSparseModel <- function(object)
 {
   zeros = which(object$coefficients==0)
   zeronames = names(object$coefficients[zeros])
+  allnames = attr(object$terms,"term.labels")
+  #zeronames = attr(object$terms,"term.labels")[zeros]
   sparse.model = ". ~ ."
-  for (n in zeronames) {
-      sparse.model = paste(sparse.model, " -", n, sep="")
+  contrastnames = names(object$contrasts)
+  namelens = sapply(contrastnames, nchar)
+  if (length(namelens) > 0) {
+    sortednames = sort(namelens, decreasing=TRUE, index.return=TRUE)
+  } else {
+    sortednames = c()
+  }
+  for (nn in zeronames) {
+    if (nn %in% allnames) {
+      # not a factor
+      sparse.model = paste(sparse.model, " -", nn, sep="")
+    } else {
+      if (nn == "(Intercept)") {
+        sparse.model = paste(sparse.model, " -1", sep="")
+      } else if (is.null(sortednames)) {
+        warning(paste(nn, " had a zero coefficient, but is not in term.labels and is not a factor.  Cannot remove it from the model."))
+      } else {
+        factor.found = FALSE
+        for (cc in contrastnames[sortednames$ix]) {
+          # go through contrastnames, starting with the longest name first
+          # for the case when one variable's name is the same as another variable's name with
+          # extra characters added on.
+          if (substr(nn,1,nchar(cc))==cc) {
+            # now make sure ALL levels with this factor are 0
+            # tricky for now just handle 2 level case (since we don't have to check any other coefficients)
+            # TODO handle more than two levels
+            sparse.model = paste(sparse.model, " -", cc, sep="")
+          }
+        }
+      }
+    }
   }
   sparse.formula = deparse(update(formula(object), sparse.model), width.cutoff=500)
   m <- lm(sparse.formula, data=object$data)
@@ -138,21 +169,48 @@ init.ones <- function(x,y)
     return(list(beta = beta, sigma = sigma))
   }
 
+init.runif <- function(x,y)
+  {
+    n = length(y)
+    beta <- as.numeric(matrix(runif(dim(x)[2]),dim(x)[2],1))
+    sigma = sqrt(sum((y - x %*% beta)^2) / n)
+    return(list(beta = beta, sigma = sigma))
+  }
+
+init.rnorm <- function(x,y)
+  {
+    n = length(y)
+    beta <- as.numeric(matrix(rnorm(dim(x)[2],0,50),dim(x)[2],1))
+    sigma = sqrt(sum((y - x %*% beta)^2) / n)
+    return(list(beta = beta, sigma = sigma))
+  }
+
 # Figueiredo EM algorithm for adaptive sparsity (Jeffreys prior)
 figEM <-
 function(x, y, init = NULL, stopDiff = 1e-8, epsilon = 1e-6, a = 1)
 {
   n = dim(x)[1]
   d = dim(x)[2]
+  if (n < d) {
+      stop(paste('number of observations or rows (',n,') is less than number of dimensions or columns (',d,')'))
+  }
 
   if(is.null(init))
   {
     init = fit.ols.lm(x,y)
     #init = init.ones(x,y)
+    #init = init.runif(x,y)
+    #init = init.rnorm(x,y)
   }
   
   beta = init$beta
-  sigmaSqr = init$sigma^2
+  # TODO ok, perhaps we can use a better estimate of variance of beta than max(abs(beta))/2
+  # also, should this somehow be related to the scale of x or y?
+  naidx = is.na(beta)
+  betavar = max(abs(beta[!naidx])) / 2
+  beta[naidx] = rnorm(length(beta[naidx]),0,betavar)
+  sigma = sqrt(sum((y - x %*% beta)^2) / length(y))
+  sigmaSqr = sigma^2
 
   diff = stopDiff + 1
   while(diff > stopDiff)
@@ -166,7 +224,8 @@ function(x, y, init = NULL, stopDiff = 1e-8, epsilon = 1e-6, a = 1)
     if (length(cols) > 0) {
       Unz <- U[cols,cols]
       xnz <- x[,cols]
-      betanz = as.numeric(Unz %*% solve(diag(a*sigmaSqr,length(cols),length(cols)) + Unz %*% t(xnz) %*% xnz %*% Unz, Unz %*% t(xnz) %*% y))
+      betanz = try(as.numeric(Unz %*% solve(diag(a*sigmaSqr,length(cols),length(cols)) + Unz %*% t(xnz) %*% xnz %*% Unz, Unz %*% t(xnz) %*% y))); if(class(betanz) == "try-error") break
+
       beta[cols] <- betanz
       beta[-cols] <- 0
     }
